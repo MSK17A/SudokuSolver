1530b3a9b32944587780aa94942affc244ce307e
M-SK <legendary512@gmail.com>
2023-03-10 17:12:26 +0300

    .

--- test/main.go
+++ test/main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"piscine"
 )
 
 func main() {
-	board := piscine.Board{
+	var board piscine.Board
+	args := os.Args[1:]
+	// Only accept args that are from 1 to 9 otherwise return
+	if len(args) < 9 {
+		fmt.Println("Error")
+		return
+	}
+	for row, value := range args {
+		if len(value) < 9 {
+			fmt.Println("Error")
+			return
+		}
+		for col, value_char := range value {
+			if '1' <= value_char && value_char <= '9' || value_char == '.' {
+				if value_char == '.' {
+					board[row][col] = 0
+				} else {
+					board[row][col] = int(value_char - '0')
+				}
+			} else {
+
+				return
+			}
+		}
+	}
+	/*board := piscine.Board{
 		{0, 9, 6, 0, 4, 0, 0, 0, 1},
 		{1, 0, 0, 0, 6, 0, 0, 0, 4},
 		{5, 0, 4, 8, 1, 0, 3, 9, 0},
@@ -15,6 +42,8 @@ func main() {
 		{0, 1, 0, 6, 3, 0, 0, 5, 9},
 		{0, 5, 9, 0, 7, 0, 8, 3, 0},
 		{0, 0, 3, 5, 9, 0, 0, 0, 7},
+	}*/
+	if !piscine.SudokuSolver(&board) {
+		fmt.Print("Error")
 	}
-	piscine.SudokuSolver(&board)
 }
